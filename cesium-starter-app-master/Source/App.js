d76c52ed056010ffc83cc12ea17872463f9d5024
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-01 15:27:30 +0900

    m

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('/Source/test.kmz'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
