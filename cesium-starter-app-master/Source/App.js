fc2d39deff85e1594ecb12de049752e15b1a3d7c
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-01 14:00:41 +0900

    b

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -10,3 +10,16 @@ point.add({
             color : Cesium.Color.YELLOW
     }
 });
+
+//add a line
+var line = viewer.entities;
+
+line.add({
+    name : 'Red line on the surface',
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
+                                                        -125, 35]),
+        width : 5,
+        material : Cesium.Color.RED
+    }
+});
