a88a80ed8cf7370254cbf1267e67b17c826e62f5
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-01 13:57:12 +0900

    change

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,20 +1,5 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-//add a kml file
-//viewer.dataSources.add(Cesium.CzmlDataSource.load('/Source/test.czml'));
-
-//add a line
-var line = viewer.entities;
-
-line.add({
-    name : 'Red line on the surface',
-    polyline : {
-        positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
-                                                        -125, 35]),
-        width : 5,
-        material : Cesium.Color.RED
-    }
-});
 
 var point = viewer.entities;
 
@@ -23,19 +8,5 @@ point.add({
         point : {
             pixelSize : 10,
             color : Cesium.Color.YELLOW
-    }
-});
-
-//add polygon
-var Polygon = viewer.entities;
-
-Polygon.add({
-    name : 'Polygon',
-    polygon : {
-        hierarchy : Cesium.Cartesian3.fromDegreesArray([-108.0, 42.0,
-                                                        -100.0, 42.0,
-                                                        -104.0, 40.0]),
-        extrudedHeight: 500000.0,
-	material : Cesium.Color.GREEN
     }
 });
