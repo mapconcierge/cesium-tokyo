b100b3ac5502b2913653108392a74b5cdace316f
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-01 15:01:33 +0900

    k to K

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.kmlDataSource.load('/Source/test.kmz'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('/Source/test.kmz'));
