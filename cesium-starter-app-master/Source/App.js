6f2ba3f2f7d4b3a6b1370fefe7d1b81caaa4d30f
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-03 16:17:03 +0900

    change

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,17 +1,17 @@
-//var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer');
 
 
-var viewer = new Cesium.Viewer('cesiumContainer', {
-imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
-credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
-}),
-baseLayerPicker: false
-});
+//var viewer = new Cesium.Viewer('cesiumContainer', {
+//imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+//url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
+//credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
+//}),
+//baseLayerPicker: false
+//});
 
 
 // 1. Fly to a position with a top-down view
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.75,35.67, 15000.0)});
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
 
-//viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
