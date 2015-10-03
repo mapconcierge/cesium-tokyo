415c886404ecc5d6a45b388678d95990876dbb2a
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-03 16:14:52 +0900

    6to5

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -11,7 +11,7 @@ baseLayerPicker: false
 
 
 // 1. Fly to a position with a top-down view
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.75,35.67, 15000.0)});
 
 
 //viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
