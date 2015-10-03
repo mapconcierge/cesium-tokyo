6ee8f456680eed4fc28e42a5138c82a50f6ecfd4
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-03 16:18:43 +0900

    地理院地図を消した

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,15 +1,6 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 
-//var viewer = new Cesium.Viewer('cesiumContainer', {
-//imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-//url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
-//credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
-//}),
-//baseLayerPicker: false
-//});
-
-
 // 1. Fly to a position with a top-down view
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
