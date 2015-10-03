bb9c1fa3a64dbe758842a71b3f7827824f254bc7
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-03 16:00:27 +0900

    視点かわるか？

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -8,10 +8,10 @@ credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/devel
 }),
 baseLayerPicker: false
 });
-viewer.scene.camera.setPositionCartographic(Cesium.Cartographic.fromDegrees(100.00, 36.14, 20000000.0));
+
 
 // 1. Fly to a position with a top-down view
-//viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
+viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
 
 //viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
