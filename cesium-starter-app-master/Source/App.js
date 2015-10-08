8f8b25b7d7455ebd4c965f7a7b7e8055306a5a32
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-08 16:38:09 +0900

    地理院タイル

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -4,5 +4,13 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 // 1. Fly to a position with a top-down view
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
+//地理院タイル読み込み
+var viewer = new Cesium.Viewer('cesiumContainer', {
+imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
+credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
+}),
+baseLayerPicker: false
+});
 
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
