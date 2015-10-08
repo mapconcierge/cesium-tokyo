904a68a740a27d299da8f803188744034b5c1083
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-08 16:48:25 +0900

    bing-none

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,14 +1,8 @@
-//var viewer = new Cesium.Viewer('cesiumContainer');
+var viewer = new Cesium.Viewer('cesiumContainer');
 
-var viewer = new Cesium.BingMapsImageryProvider({
-    url : '//dev.virtualearth.net',
-    key : 'get-yours-at-https://www.bingmapsportal.com/',
-    mapStyle : Cesium.BingMapsStyle.AERIAL
-});
 
 // 1. Fly to a position with a top-down view
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
-
 
 
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
