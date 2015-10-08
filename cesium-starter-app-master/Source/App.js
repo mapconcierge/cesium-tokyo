122239f157dd89fa330c168f02a89317a2816386
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-08 18:45:14 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -6,13 +6,3 @@ Cesium.BingMapsApi.defaultKey = "";
       }),
       baseLayerPicker: false
     });
-
-
-//var viewer = new Cesium.Viewer('cesiumContainer');
-
-
-// 1. Fly to a position with a top-down view
-viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
-
-
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://sagara1020.github.io/cesium-tokyo/cesium-starter-app-master/Source/test.kml'));
