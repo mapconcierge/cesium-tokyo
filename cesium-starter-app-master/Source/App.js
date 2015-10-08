8884aa7ccee5c8e9266ee140c65fdbb052b04893
sagara1020 <yamauchikazuyuki@sannai-no-MacBook-Air-2.local>
2015-10-08 19:04:15 +0900

    git

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -7,6 +7,44 @@ Cesium.BingMapsApi.defaultKey = "";
       baseLayerPicker: false
     });
 
+    //ポイントを追加
+    var point = viewer.entities;
+
+    point.add({
+            position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+            point : {
+                pixelSize : 10,
+                color : Cesium.Color.YELLOW
+        }
+    });
+
+    //ラインを追加
+    var line1 = viewer.entities;
+
+    line1.add({
+        name : 'Red line on the surface',
+        polyline : {
+            positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
+                                                            -125, 35]),
+            width : 5,
+            material : Cesium.Color.RED
+        }
+    });
+
+    //ポリゴンを追加
+    var Polygon = viewer.entities;
+
+    Polygon.add({
+        name : 'Polygon',
+        polygon : {
+            hierarchy : Cesium.Cartesian3.fromDegreesArray([-108.0, 42.0,
+                                                            -100.0, 42.0,
+                                                            -104.0, 40.0]),
+            extrudedHeight: 500000.0,
+    	material : Cesium.Color.GREEN
+        }
+    });
+
 //視点の変更
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
